21b64a9424e793da1d2705e346c5c70c8d8e34cf
claytonoliveira <clayton@devplus.com.br>
2025-04-06 18:50:39 -0300

    feat: Adicionar suporte a MaxRetry na interface IMessagingConsumer e atualizar implementação no TestConsumer

--- Devplus.Messaging/src/Interfaces/IMessagingConsumer.cs
+++ Devplus.Messaging/src/Interfaces/IMessagingConsumer.cs
@@ -3,8 +3,27 @@ using Devplus.Messaging.Models;
 namespace Devplus.Messaging.Interfaces;
 public interface IMessagingConsumer
 {
-    string QueueName { get; } // Fila de consumo
-    string ExchangeName { get; } // Exchange da qual a mensagem será recebida
-    string RoutingKey { get; } // Chave de roteamento para o binding
+    /// <summary>
+    /// Fila de consumo
+    /// </summary>
+    string QueueName { get; }
+    /// <summary>
+    /// Exchange da qual a mensagem será recebida
+    /// </summary>
+    string ExchangeName { get; }
+    /// <summary>
+    /// Chave de roteamento para o binding
+    /// </summary>
+    string RoutingKey { get; }
+    /// <summary>
+    /// Total de tentativas de reprocessamento
+    /// </summary>
+    int MaxRetry { get; }
+    /// <summary>
+    /// Método de consumo 
+    /// </summary>
+    /// <param name="cloudEvent"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     Task ConsumeAsync(CloudEvent<object> cloudEvent, CancellationToken cancellationToken);
 }
