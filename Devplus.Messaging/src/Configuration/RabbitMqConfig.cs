5e5762ec8c6781edccd9cd70a173c3f5f7bfa9e9
Clayton Oliveira <clayton@devplus.com.br>
2025-09-29 10:55:44 -0300

    feat: Atualizar versão do pacote para 2.7.5 e ajustar configurações padrão do RabbitMqConfig

--- Devplus.Messaging/src/Configuration/RabbitMqConfig.cs
+++ Devplus.Messaging/src/Configuration/RabbitMqConfig.cs
@@ -2,11 +2,11 @@ namespace Devplus.Messaging.Configuration;
 
 public class RabbitMqConfig
 {
-    public string Host { get; set; }
+    public string Host { get; set; } = "localhost";
     public int Port { get; set; } = 5672;
     public string UserName { get; set; } = "guest";
     public string Password { get; set; } = "guest";
-    public string VHost { get; set; }
-    public ushort GlobalPrefetchCount { get; set; } = 3;
-    public bool UseGlobalPrefetch { get; set; } = true;
+    public string VHost { get; set; } = "/";
+    public ushort GlobalPrefetchCount { get; set; } = 0;
+    public bool UseGlobalPrefetch { get; set; } = false;
 }
